a030d03fcf9181c0931e6c30a7cbbe0374f87c20
Francisco Emilio Cavenaghi <franchucave@hotmail.com>
2022-03-20 19:12:51 -0300

    Added static video to cameras.

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -1,16 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CameraManager : MonoBehaviour
 {
     [SerializeField]GameObject[] cameras;
     [SerializeField] int currentCamera;
     [SerializeField] GameObject player;
+    [SerializeField] Text text;
+    [SerializeField]GameObject staticVideo;
+    [SerializeField] float seconds;
+    [SerializeField] bool startSeconds;
     // Start is called before the first frame update
     void Start()
     {
-        currentCamera = 0;
+        
     }
 
     // Update is called once per frame
@@ -24,6 +29,7 @@ public class CameraManager : MonoBehaviour
                 currentCamera = cameras.Length-1;
             }
             ActiveCamera();
+            startSeconds = true;
         }
         else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
@@ -33,11 +39,23 @@ public class CameraManager : MonoBehaviour
                 currentCamera = 0;
             }
             ActiveCamera();
+            startSeconds = true;
+        }
+        if (startSeconds)
+        {
+            seconds += Time.deltaTime;
+        }
+        if(seconds > 0.2)
+        {
+            staticVideo.SetActive(false);
+            startSeconds = false;
+            seconds = 0;
         }
     }
 
     void ActiveCamera()
     {
+        staticVideo.SetActive(true);
         foreach (GameObject camera in cameras)
         {
             camera.SetActive(false);
@@ -51,5 +69,36 @@ public class CameraManager : MonoBehaviour
                 player.SetActive(true);
             }
         }
+        switch (currentCamera)
+        {
+            case 0:
+                text.text = "Player큦 View";
+                break;
+            case 1:
+                text.text = "Mercury큦 View";
+                break;
+            case 2:
+                text.text = "Venus큦 View";
+                break;
+            case 3:
+                text.text = "Earth큦 View";
+                break;
+            case 4:
+                text.text = "Mars큦 View";
+                break;
+            case 5:
+                text.text = "Jupiter큦 View";
+                break;
+            case 6:
+                text.text = "Saturn큦 View";
+                break;
+            case 7:
+                text.text = "Uranus큦 View";
+                break;
+            case 8:
+                text.text = "Neptune큦 View";
+                break;
+        }
     }
+   
 }
